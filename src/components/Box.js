65088623a1e3a7e338cd0a2dd099ec695ac52376
Nothdurftm17 <97467294+Nothdurftm17@users.noreply.github.com>
2022-03-08 20:43:00 -0500

    first commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 
 class PersonCard extends Component{
     constructor(props){
-        super(props);
+        super(props)
+        this.state = {
+            age : props.age
+        }
     }
-
+    updateState = () => {
+        this.setState({age:this.state.age + 1})
+    }
     render(){
-        const{first_name, last_name, age, hairColor} = this.props;
+        const{first_name, last_name, hairColor} = this.props;
+
         return(
             <div>
                 <h1>{last_name},{first_name}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick = {this.updateState}>Birthday Button for {first_name} {last_name}</button>
             </div>
+            
         )
     }
 }
